de519eb109dcefb7f792c6a6610d2d7f81df220f
zero <940034240@qq.com>
2018-11-19 15:38:49 +0800

    网页端，显示昵称和头像

---
+++ WebProject/VoiceNote/src/main/app/HeadIconBase64Get.java
@@ -0,0 +1,49 @@
+package main.app;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.annotation.WebServlet;
+
+import datasourse.DBUtils;
+import main.ResponseParams;
+import main.User;
+import main.base.BaseServlet;
+import main.util.ImageUtil;
+import main.util.ObjUtils;
+
+@WebServlet("/HeadIconBase64Get")
+public class HeadIconBase64Get extends BaseServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public ResponseParams doSQL(Map<String, String> params, DBUtils db, User user) {
+		String filepath = "C:\\VoiceNote\\" + ObjUtils.objToStr(params.get("name")) + "\\icon.jpg";
+		File file = new File(filepath);
+		if (!file.exists()) {
+			file = new File( "C:\\VoiceNote\\icon.png");
+		}
+		boolean hasFile = true;
+		if (!file.exists()) {
+			file = null;
+			hasFile = false;
+		}
+		String nickname = ObjUtils.objToStr(params.get("nickname"));
+		if (nickname.equals("")) {
+			nickname = ObjUtils.objToStr(params.get("name"));
+		}
+		System.out.println("nickname:" + nickname);
+
+		Map<String, Object> map = new HashMap<>();
+		map.put("nickname", nickname);
+		map.put("hasFile", hasFile);
+		map.put("file", ImageUtil.getImageStr(file.getAbsolutePath()));
+		// ResponseUtil.responseFile(response, null, map, nickname, file);
+		ResponseParams responseParams = new ResponseParams();
+		responseParams.resultMap = map;
+		responseParams.msg = nickname;
+		return responseParams;
+	}
+
+}
---
+++ WebProject/VoiceNote/src/main/util/ImageUtil.java
@@ -0,0 +1,31 @@
+package main.util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import sun.misc.BASE64Encoder;
+
+public  class ImageUtil {
+	/**
+	 * @Description: 根据图片地址转换为base64编码字符串
+	 * @Author: 
+	 * @CreateTime: 
+	 * @return
+	 */
+	public static String getImageStr(String imgFile) {
+	    InputStream inputStream = null;
+	    byte[] data = null;
+	    try {
+	        inputStream = new FileInputStream(imgFile);
+	        data = new byte[inputStream.available()];
+	        inputStream.read(data);
+	        inputStream.close();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	    // 加密
+	    BASE64Encoder encoder = new BASE64Encoder();
+	    return encoder.encode(data);
+	}
+}
